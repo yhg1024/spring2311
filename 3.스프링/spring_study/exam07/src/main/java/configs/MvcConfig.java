ce45335abb03df4544005070635af97ae25ecca4
yhg1024 <dgr1024kr@naver.com>
2023-12-14 16:35:17 +0900

    타임리프

--- 3.스프링/spring_study/exam07/src/main/java/configs/MvcConfig.java
+++ 3.스프링/spring_study/exam07/src/main/java/configs/MvcConfig.java
@@ -1,10 +1,13 @@
 package configs;
 
+import commons.Utils;
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.*;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -28,6 +31,12 @@ public class MvcConfig implements WebMvcConfigurer { // 주요 Mvc 설정 항목
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**") // /** 기본경로에서 하위경로를 포함한 모든 경로
                 .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+                .setViewName("main/index");
     }
 
     @Bean
@@ -64,5 +73,19 @@ public class MvcConfig implements WebMvcConfigurer { // 주요 Mvc 설정 항목
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.viewResolver(thymeleafViewResolver());
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setDefaultEncoding("UTF-8");
+        ms.setBasenames("messages.commons");
+
+        return ms;
+    }
+
+    @Bean
+    public Utils utils() {
+        return new Utils();
     }
 }
--- 3.스프링/spring_study/exam07/src/main/java/controllers/member/MemberController.java
+++ 3.스프링/spring_study/exam07/src/main/java/controllers/member/MemberController.java
@@ -1,14 +1,28 @@
 package controllers.member;
 
+import models.member.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class MemberController {
 
     @GetMapping("/member/join")
-    public String join() {
+    public String join(Model model) {
+
+        String[] addCss = {"member/style1", "member/style2"};
+        List<String> addScript = Arrays.asList("member/script1", "member/script2");
+
+        model.addAttribute("addCss", addCss);
+        model.addAttribute("addScript", addScript);
+        model.addAttribute("pageTitle", "회원가입");
 
         return "member/join";
     }
@@ -30,5 +44,24 @@ public class MemberController {
         System.out.println(form);
 
         return "member/login";
+    }
+
+    @GetMapping("/member/list")
+    public String members(Model model) {
+        List<Member> members = new ArrayList<>();
+        for (int i = 1; i <= 10; i++){
+            Member member = Member.builder()
+                    .userNo(Long.valueOf(i))
+                    .userPw("1234")
+                    .userId("user" + i)
+                    .userNm("사용자" + i)
+                    .email("user" + i + "@test.org")
+                    .regDt(LocalDateTime.now())
+                    .build();
+            members.add(member);
+        }
+        model.addAttribute("members", members);
+
+        return "member/list";
     }
 }
