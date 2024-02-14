67094b3a2e21a9107a2a725ffc84d2050e77b58f
yhg1024 <dgr1024kr@naver.com>
2024-02-14 10:35:22 +0900

    리액트

--- 7.리액트/exam03/src/components/commons/StyledButton.js
+++ 7.리액트/exam03/src/components/commons/StyledButton.js
@@ -1,7 +1,19 @@
 import styled from "styled-components";
+import { buttonSizes } from "../../style/size";
 
-export const SmallButton = styled.button``;
+const { small, medium, big } = buttonSizes;
 
-export const MediumButton = styled.button``;
+export const SmallButton = styled.button`
+  width: ${small.width}px;
+  height: ${small.height}px;
+`;
 
-export const BigButton = styled.button``;
+export const MediumButton = styled.button`
+  width: ${medium.width}px;
+  height: ${medium.height}px;
+`;
+
+export const BigButton = styled.button`
+  width: ${big.width}px;
+  height: ${big.height}px;
+`;
---
+++ 7.리액트/exam03/src/style/size.js
@@ -0,0 +1,5 @@
+const buttonSizes = {
+  small: { width: 150, height: 30 },
+  medium: { width: 250, height: 45 },
+  big: { width: 350, height: 55 },
+};
